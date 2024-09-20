9a2e1efe4b94f64110be9ca12c2d96ddb27eb798
3mpty <aurelien.corre@outlook.fr>
2024-09-20 22:24:46 +0200

    Test with a loading page

---
+++ src/components/loading/Loading.tsx
@@ -0,0 +1,36 @@
+"use client";
+import React, { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import styles from "../../app/page.module.scss";
+
+const LoadingPage: React.FC = () => {
+    const circleRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const tl = gsap.timeline({
+            repeat: -1,
+            yoyo: true,
+            repeatDelay: 0.5,
+        });
+
+        tl.to(circleRef.current, {
+            scale: 5,
+            rotate: 360,
+            duration: 1.5,
+            ease: "power1.inOut",
+        }).to(circleRef.current, {
+            scale: 1,
+            rotate: 0,
+            duration: 1.5,
+            ease: "power1.inOut",
+        });
+    }, []);
+
+    return (
+        <div className={styles.loadingContainer}>
+            <div ref={circleRef} className={styles.circle} />
+        </div>
+    );
+};
+
+export default LoadingPage;
